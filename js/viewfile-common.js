4d789af798114bc63d7e1422af0c91d1829f4d25
IAmMoltony <koronavirusnyj@gmail.com>
2024-01-22 21:21:43 +0600

    Add function for getting raw file url

--- js/viewfile-common.js
+++ js/viewfile-common.js
@@ -6,3 +6,11 @@
     }
     pFileName.innerText = fileNameText;
 })();
+
+const getRawFileUrl = () => {
+    let rawFileUrl = `/rawFile?name=${getQueryParam('name')}`;
+    if (getQueryParam('folder') != undefined) {
+        rawFileUrl += `&folder=${getQueryParam('folder')}`;
+    }
+    return rawFileUrl;
+}
